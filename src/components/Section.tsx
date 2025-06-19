95061669b3d6657140f6b2ba16885ac03557f81d
MD Suweb Reza <swebreza@gmail.com>
2025-06-19 18:56:03 +0530

    Enhance homepage layout and update Section component
    - Redesigned the homepage to feature a gradient background and improved card styles for program offerings.
    - Added new interactive elements and icons to enhance user engagement.
    - Updated the Section component to accept ReactNode for title and description, allowing for more flexible content rendering.
    - Improved accessibility with appropriate aria-labels for interactive elements.

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,10 @@
-import SectionTitle from "./SectionTitle";
+import SectionTitle from './SectionTitle'
+import { ReactNode } from 'react'
 
 interface Props {
-  id: string;
-  title: string;
-  description: string;
+  id: string
+  title: ReactNode
+  description: ReactNode
 }
 
 const Section: React.FC<React.PropsWithChildren<Props>> = ({
@@ -13,14 +14,14 @@ const Section: React.FC<React.PropsWithChildren<Props>> = ({
   children,
 }: React.PropsWithChildren<Props>) => {
   return (
-    <section id={id} className="py-10 lg:py-20">
+    <section id={id} className='py-10 lg:py-20'>
       <SectionTitle>
-        <h2 className="text-center text-[#bf5104] mb-4">{title}</h2>
+        <h2 className='text-center text-[#bf5104] mb-4'>{title}</h2>
       </SectionTitle>
-      <p className="mb-12 text-center font-bold italic ">{description}</p>
+      <p className='mb-12 text-center font-bold italic '>{description}</p>
       {children}
     </section>
-  );
-};
+  )
+}
 
-export default Section;
+export default Section
